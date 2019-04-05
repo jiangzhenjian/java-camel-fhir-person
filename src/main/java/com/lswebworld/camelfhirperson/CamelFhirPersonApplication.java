b4bee3075cd5ac64a6244605b98e2859d21ff849
Larry Shields <shieldsl@upmc.edu>
2019-04-05 10:27:31 -0400

    Project Setup

---
+++ src/main/java/com/lswebworld/camelfhirperson/CamelFhirPersonApplication.java
@@ -0,0 +1,13 @@
+package com.lswebworld.camelfhirperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelFhirPersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelFhirPersonApplication.class, args);
+	}
+
+}
